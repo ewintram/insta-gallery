e3cb7e1c4879b5855491713323068b3207c2a005
Eleanor Wintram <eleanorwintram@gmail.com>
2018-02-25 16:49:47 +0000

    tests App renders Posts component

--- src/__tests__/App.test.js
+++ src/__tests__/App.test.js
@@ -1,14 +1,27 @@
+import TestUtils from 'react-dom/test-utils';
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from '../App';
+import Posts from '../Posts';
 import data from '../__mocks__/data.json';
 import { shallow } from 'enzyme';
+jest.mock('../Posts');
+
+var component
+var childComponent
 
 it('renders without crashing', () => {
   const div = document.createElement('div');
   ReactDOM.render(<App posts={data}/>, div);
   ReactDOM.unmountComponentAtNode(div);
 });
+
+it('renders the Posts component', function() {
+  component = TestUtils.renderIntoDocument(<App posts={data}/>);
+  childComponent = TestUtils.scryRenderedComponentsWithType(component, Posts);
+  expect(childComponent.length).toEqual(1);
+});
+
 
 describe('removePost', () => {
   it('removes the post at the specified position in the array', () => {
