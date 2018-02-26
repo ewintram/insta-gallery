f32f9ce549c5c64f3569abdbb6f6c6f7d061c5c8
Eleanor Wintram <eleanorwintram@gmail.com>
2018-02-26 10:26:49 +0000

    adds titles to UserComment component

--- src/__tests__/UserComment.test.js
+++ src/__tests__/UserComment.test.js
@@ -6,5 +6,5 @@ import { shallow } from 'enzyme';
 it('renders the comment text and username', () => {
   const wrapper = shallow(<UserComment comment={data[0]}/>);
   const comment = wrapper.find('li')
-  expect(comment.html()).toEqual("<li>heacan<br/>comment 1</li>");
+  expect(comment.html()).toEqual("<li><p>Username</p>heacan<br/><p>Comment</p>comment 1</li>");
 });
--- src/components/UserComment.js
+++ src/components/UserComment.js
@@ -4,7 +4,9 @@ class UserComment extends React.Component {
   render() {
     return (
       <li>
+        <p>Username</p>
         {this.props.comment.username}<br />
+        <p>Comment</p>
         {this.props.comment.text}
       </li>
     )
