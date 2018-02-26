0cda670944af072ff6a8ae09134013d30f98e2f4
Eleanor Wintram <eleanorwintram@gmail.com>
2018-02-26 11:39:17 +0000

    fixes CommentsForm test following CSS updates

--- src/__tests__/CommentsForm.test.js
+++ src/__tests__/CommentsForm.test.js
@@ -2,8 +2,14 @@ import React from 'react';
 import CommentsForm from '../components/CommentsForm';
 import { shallow } from 'enzyme';
 
+var prop
+var wrapper
+
+beforeEach(() => {
+  wrapper = shallow(<CommentsForm addComment={prop}/>);
+});
+
 it('renders the comments form', () => {
-  const wrapper = shallow(<CommentsForm />);
   const form = wrapper.find('form')
-  expect(form.html()).toEqual("<form><div class=\"form-group\"><label for=\"formBasicText\" class=\"control-label\">Username</label><input type=\"text\" placeholder=\"Please leave your username\" id=\"formBasicText\" class=\"form-control\"/><label for=\"formBasicText\" class=\"control-label\">Comment</label><textarea placeholder=\"Please leave your comment\" id=\"formBasicText\" class=\"form-control\"></textarea></div><button type=\"submit\" class=\"btn btn-default\">Submit</button></form>");
+  expect(form.html()).toEqual("<form class=\"commentsForm\"><div class=\"form-group\"><label for=\"formBasicText\" class=\"control-label\">Username</label><input type=\"text\" placeholder=\"Please leave your username\" id=\"formBasicText\" class=\"form-control\"/><label for=\"formBasicText\" class=\"control-label\">Comment</label><textarea placeholder=\"Please leave your comment\" id=\"formBasicText\" class=\"form-control\"></textarea></div><button type=\"submit\" class=\"submitComment btn btn-default\">Submit</button></form>");
 });
